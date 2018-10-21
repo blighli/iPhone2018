0bdf26cfe9ff3cdf550b66130c3dfba24c5ae4eb
Dawn <lu923418335@gmail.com>
2018-10-21 22:21:32 +0800

    上传第一次作业

---
+++ 21851134鹿皓/project01/CalPrinter.h
@@ -0,0 +1,19 @@
+//
+//  CalPrinter.h
+//  Cal
+//
+//  Created by dawn on 2018/10/19.
+//  Copyright © 2018 dawn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CalPrinter : NSObject
+
+-(void)prin:(int)year and:(int)month and:(int)day;
+
+@end
+
+NS_ASSUME_NONNULL_END
