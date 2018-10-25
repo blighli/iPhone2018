8e0383e645bd534a136f0ae766b0df238b33cb55
zhenyuczy <zhenyuczy@163.com>
2018-10-26 03:39:32 +0800

    Project2 completed by Zhenyu Chen

---
+++ 21851109陈振宇/Project2/Project2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project2
+//
+//  Created by Zhenyu Chen on 2018/10/25.
+//  Copyright © 2018年 Zhenyu Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
