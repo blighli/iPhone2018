ad20ac2d88f8eeb5029b0d73b5fe090f409ada3d
shayue <t.t.luokeke@qq.com>
2018-10-31 00:17:01 +0800

    yes

---
+++ 王涛21851450/第二次作业/iTahDoodle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTahDoodle
+//
+//  Created by shayue on 2018/10/25.
+//  Copyright © 2018 shayue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
