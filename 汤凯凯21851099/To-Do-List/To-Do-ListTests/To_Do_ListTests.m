a427e97dc02b39a7476d7333ad41e66c0932bd5b
Kaikai Tang <tangkaikai@hyperchain.cn>
2018-10-31 15:28:17 +0800

    feat(To-Do-List): implements To-Do-List application

---
+++ 汤凯凯21851099/To-Do-List/To-Do-ListTests/To_Do_ListTests.m
@@ -0,0 +1,37 @@
+//
+//  To_Do_ListTests.m
+//  To-Do-ListTests
+//
+//  Created by 汤凯凯 on 2018/10/29.
+//  Copyright © 2018年 汤凯凯. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface To_Do_ListTests : XCTestCase
+
+@end
+
+@implementation To_Do_ListTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
