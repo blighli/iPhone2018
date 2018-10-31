d35f5b53cdb6ed9c1bb708c42427dc70780de032
HuaDream <huadream@HuaDreamdeMBP.lan>
2018-10-31 16:23:08 +0800

    add TaskList

---
+++ 周世鹏21851175/TaskList/TaskList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskList
+//
+//  Created by HuaDream on 2018/10/29.
+//  Copyright © 2018 HuaDream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
