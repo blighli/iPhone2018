038802dc7d395dc459548101cbb77cff681c3d40
jhjCN <504525127@qq.com>
2018-12-17 22:56:59 +0800

    Add files via upload

---
+++ 季豪杰21851468/ios大作业/EnglishHelper/EnglishHelper/Support/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EnglishHelper
+//
+//  Created by pc－jhj on 2018/12/13.
+//  Copyright © 2018年 jhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+ 
