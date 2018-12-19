2545bbb9a7230e03e29dbf140d8a646bced08ba0
OBJC <44425344+OBJCkehaowanle@users.noreply.github.com>
2018-12-19 23:54:57 +0800

    21851161赵帅_大作业 提交1

---
+++ 21851161赵帅_大作业/TravelJournal/PocketDiaryTests/PocketDiaryTests.m
@@ -0,0 +1,40 @@
+//
+//  PocketDiaryTests.m
+//  PocketDiary
+//
+//  Created by 赵帅 on 2018/11/18.
+//  Copyright © 2018 赵帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PocketDiaryTests : XCTestCase
+
+@end
+
+@implementation PocketDiaryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
