7a1d85a560e68ec743226262a741bd1263749977
toFindMore <17855824057@163.com>
2018-12-20 19:35:15 +0800

    平时作业

---
+++ 周健21851418/homework2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhomework2
+//
+//  Created by shitailong on 2018/12/20.
+//  Copyright © 2018 shitailong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
