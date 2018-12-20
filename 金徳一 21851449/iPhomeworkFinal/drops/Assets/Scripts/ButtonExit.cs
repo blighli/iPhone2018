676386b545b8439c108bbd2f200a269bdf2dd373
yi <289585274@qq.com>
2018-12-20 21:38:25 +0800

    iPhomeworkFinal
    drop v0.4

---
+++ 金徳一 21851449/iPhomeworkFinal/drops/Assets/Scripts/ButtonExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+}
